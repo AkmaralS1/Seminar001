f2581a27517726dc6f3eac98a498a882e013e948
Akmaral <axma.mail@gmail.com>
2022-10-01 22:25:20 +0600

    Project002

---
+++ Project002/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите первое число: ");
+int xa = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int xb = Convert.ToInt32(Console.ReadLine());
+if (xb == xa * xa)
+    Console.WriteLine("Второе число является квадратом первого");
+else
+    Console.WriteLine("Второе число не является квадратом первого");  
